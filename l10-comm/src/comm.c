fd9f836bf1dfb550768bd6899e2e66d1a578b5a6
Karenw1004 <47468628+Karenw1004@users.noreply.github.com>
2019-11-21 09:02:53 -0600

    Update comm.c

--- l10-comm/src/comm.c
+++ l10-comm/src/comm.c
@@ -3,6 +3,21 @@
 #include <string.h>
 
 #include "eecs388_lib.h"
+#define SERVO_PULSE_MAX 2400    /* 2400 us */
+#define SERVO_PULSE_MIN 544     /* 544 us */
+#define SERVO_PERIOD    20000   /* 20000 us (20ms) */
+
+void servo(int gpio, int pos)
+{
+    //the formula of pulse
+    int pulse = (SERVO_PULSE_MIN + ( (SERVO_PULSE_MAX - SERVO_PULSE_MIN) /180)*pos);
+    //switching on the PIN_19 for a pulse amount of time
+    gpio_write(gpio,ON);
+    delay_usec(pulse);
+    //switching off the PIN_19 for a SERVO_PERIOD - pulse amount of time
+    gpio_write(gpio,OFF);
+    delay_usec(SERVO_PERIOD - pulse);
+}
 
 int main()
 {
@@ -13,8 +28,8 @@ int main()
     printf("Setup completed.\n");
     printf("Begin the main loop.\n");
 
-    int prev_dist = 0;
-    int prev_angle = 0; 
+    //int prev_dist = 0;
+    //int prev_angle = 0; 
 
     //Lab2: Blinking Led
     //declare array
@@ -27,7 +42,8 @@ int main()
     for (int i = 0 ; i < 3 ; i++){
         gpio_mode(ledArr[i], OUTPUT);
     }
-
+    //Generate a pwm signal , @gpio   gpio number, @pos    degree [0,180]
+    int dist ;
     while (1) {
 
         // Receive the data(steering angle) from pi 4 channel
@@ -39,56 +55,45 @@ int main()
 
             //Lab4 : TfLidar
             //read a data frame from the TFmini sensor
-            if ('Y' == ser_read() && 'Y' == ser_read()) {
+            if ('Y' == ser_read(0) && 'Y' == ser_read(0)) {
                 //store the Dist_L and Dist_H in a variable by ser_read()
-                int16_t d1 = ser_read();
-                int16_t d2 = ser_read();
+                int16_t d1 = ser_read(0);
+                int16_t d2 = ser_read(0);
                 //formula to make the distance into a 16 bit
                 dist = (d2 << 8) | d1;
                 printf("distance is %d",dist);
                 
                 for (int i = 0 ; i < 5 ; i++){
                 // ser_read() 5 times to read data frame that is not used in lab
-                ser_read();
+                ser_read(0);
                 }               
             }
             //turn on the red light if the distance is less than 50cm
             if (dist < 50) {
-                allLeds(OFF);
+                gpio_write(ledArr[0], OFF);
+                gpio_write(ledArr[1], OFF);
+                gpio_write(ledArr[2], OFF);
                 gpio_write(ledArr[1],ON); // red ==stop  
                 break;
             }
 
-            printf("pos: %d (degree)\n", pos);
             /* control the servor for 1 sec duration */
-            if (pos < (-30) ) {
-                allLeds(OFF);
-                gpio_write(gpio[2], ON); //blue==left
-            } else if (pos > (30)) {
-                allLeds(OFF);
-                gpio_write(gpio[0], ON);//green==right
+            if (angle < (-30) ) {
+                gpio_write(ledArr[0], OFF);
+                gpio_write(ledArr[1], OFF);
+                gpio_write(ledArr[2], OFF);
+                gpio_write(ledArr[2], ON); //blue==left
+            } else if (angle > (30)) {
+                gpio_write(ledArr[0], OFF);
+                gpio_write(ledArr[1], OFF);
+                gpio_write(ledArr[2], OFF);
+                gpio_write(ledArr[0], ON);//green==right
             } else {
-                allLeds(ON);//green==right
+                gpio_write(ledArr[0], ON);
+                gpio_write(ledArr[1], ON);
+                gpio_write(ledArr[2], ON);//white==center
             }           
         }
     }
-}
-void allLeds(int mode ){
-    gpio_write(gpio[0], mode);
-    gpio_write(gpio[1], mode);
-    gpio_write(gpio[2], mode);
-}
-
-//Generate a pwm signal , @gpio   gpio number, @pos    degree [0,180]
-void servo(int gpio, int pos)
-{
-    //the formula of pulse
-    int pulse = (SERVO_PULSE_MIN + ( (SERVO_PULSE_MAX - SERVO_PULSE_MIN) /180)*pos);
-    //switching on the PIN_19 for a pulse amount of time
-    gpio_write(gpio,ON);
-    delay_usec(pulse);
-    //switching off the PIN_19 for a SERVO_PERIOD - pulse amount of time
-    gpio_write(gpio,OFF);
-    delay_usec(SERVO_PERIOD - pulse);
 }
 
